3c41f16bec6911a45e526a4660d0b05c8598a541
YANDEX2 <vp4997043@gmail.com>
2025-05-13 20:16:01 +0300

    like and presentation

--- web_site.py
+++ web_site.py
@@ -1,3 +1,6 @@
+import random
+
+import flask_login
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField, TextAreaField
@@ -9,6 +12,8 @@ from data import db_session
 from data.users import User
 from data.blogs import Blog
 from data.likes import Like
+
+from sqlalchemy.sql.expression import func
 
 db_session.global_init("db/blogs.db")
 
@@ -22,12 +27,20 @@ login_manager.init_app(app)
 
 
 def add_user(name, position, email, password):
-    user = User()
-    user.name = name
-    user.about = position
-    user.email = email
-    user.set_password(password)
-    db_sess.add(user)
+    user1 = User()
+    user1.name = name
+    user1.about = position
+    user1.email = email
+    user1.set_password(password)
+    db_sess.add(user1)
+    db_sess.commit()
+    bl = Blog()
+    us = db_sess.query(User).filter(User.email == email).first()
+    bl.tutle = us.id
+    bl.content = "42"
+    bl.user_id = us.id
+    bl.user = us
+    us.blogs.append(bl)
     db_sess.commit()
 
 
@@ -59,6 +72,15 @@ class UserForm(FlaskForm):
     submit = SubmitField('Новый блог')
 
 
+class BlogForm(FlaskForm):
+    text = TextAreaField("Текст блога")
+    submit = SubmitField('Изменить')
+
+
+class OtherBlogForm(FlaskForm):
+    submit = SubmitField('Понравилось')
+
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess1 = db_session.create_session()
@@ -79,13 +101,13 @@ def reqister():
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
-        user = User(
+        user_ = User(
             name=form.name.data,
             email=form.email.data,
             about=form.about.data
         )
-        user.set_password(form.password.data)
-        db_sess.add(user)
+        user_.set_password(form.password.data)
+        db_sess.add(user_)
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
@@ -96,10 +118,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         db_sess1 = db_session.create_session()
-        user = db_sess1.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/logout")
+        user2 = db_sess1.query(User).filter(User.email == form.email.data).first()
+        if user2 and user2.check_password(form.password.data):
+            login_user(user2, remember=form.remember_me.data)
+            return redirect("/" + flask_login.current_user.name)
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
@@ -110,30 +132,96 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect("/")
+    return redirect("/login")
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    form =
-    return render_template('index.html', title='Блоги', form=form)
+    if flask_login.current_user:
+        db_sess1 = db_session.create_session()
+        blogs = []
+        likes = flask_login.current_user.liked_blogs
+        for i in range(10):
+            if random.randint(0, 1):
+                if likes:
+                    blog_num = random.randint(0, len(likes))
+                    if random.randint(0, 1):
+                        blogs.append(random.choice(likes[blog_num].blog.user.blogs))
+                    elif random.randint(0, 1):
+                        a = random.choice(likes[blog_num].blog.user.liked_blogs)
+                        blogs.append(a.blog)
+                    else:
+                        a = random.choice(likes[blog_num].blog.likes)
+                        blogs.append(random.choice(a.user.liked_blogs))
+                else:
+                    blogs.append(random.choice(db_sess1.query.order_by(func.random()).first()))
+            else:
+                blogs.append(random.choice(db_sess1.query.order_by(func.random()).first()))
+        return render_template('index.html', title='Блоги')
+    else:
+        return redirect("/login")
 
 
 @app.route('/<path:n>', methods=['GET', 'POST'])
 def user(n):
     n = n.split("/")
     db_sess1 = db_session.create_session()
-    user = db_sess1.query(User).filter(User.name == n[1]).first()
-    if user:
+    user3 = db_sess1.query(User).filter(User.name == n[0]).first()
+    if user3:
         if len(n) == 1:
             form = UserForm()
-            return render_template('user.html', title=n[1], name=n[1], your_page=, form=form)
+            if form.validate_on_submit():
+                if not db_sess1.query(Blog).filter(Blog.tutle == form.blog.data).first():
+                    blog = Blog()
+                    blog.title = form.blog.data
+                    flask_login.current_user.blogs.append(blog)
+                    db_sess1.merge(flask_login.current_user)
+                    db_sess1.commit()
+                    return redirect('/' + user3.name + '/' + form.blog.data)
+                else:
+                    return render_template('user.html', title=n[0], user=user3,
+                                           your_page=flask_login.current_user == user3,
+                                           message="Такой блог уже существует", form=form,
+                                           cur_user=flask_login.current_user)
+            return render_template('user.html', title=n[0], user=user3,
+                                   form=form, cur_user=flask_login.current_user)
         else:
-            blog = db_sess1.query(Blog).filter(Blog.tutle == n[2]).first()
-            if blog and blog.user == user:
-                form =
-                return render_template('blog.html', title=n[1], form=form)
+            blog = db_sess1.query(Blog).filter(Blog.tutle == n[1]).first()
+            if blog and blog.user == user3:
+                if flask_login.current_user == user3:
+                    form = BlogForm()
+                    if form.validate_on_submit():
+                        blog.content = form.text.data
+                        db_sess1.commit()
+                        return redirect('/' + flask_login.current_user.name)
+                    return render_template('blog.html', title=n[0], blog=blog,
+                                           form=form, cur_user=flask_login.current_user)
+                else:
+                    form = OtherBlogForm()
+                    like = db_sess1.query(Like).filter(Like.user == flask_login.current_user and Like.blog ==
+                                                       blog).first()
+                    if form.validate_on_submit():
+                        if like:
+                            db_sess1.delete(like)
+                            db_sess1.commit()
+                        else:
+                            like = Like()
+                            like.user_id = flask_login.current_user.id
+                            like.user = flask_login.current_user
+                            like.blog = blog
+                            like.blog_id = blog.id
+                            flask_login.current_user.liked_blogs.append(like)
+                            blog.likes.append(like)
+                            db_sess1.merge(flask_login.current_user)
+                            db_sess1.merge(blog)
+                            db_sess1.commit()
+                    if like:
+                        form.submit.label = "Не нравится"
+                    else:
+                        form.submit.label = "Нравится"
+                    return render_template('blog.html', title=n[0], blog=blog,
+                                           form=form, cur_user=flask_login.current_user)
 
 
 if __name__ == '__main__':
